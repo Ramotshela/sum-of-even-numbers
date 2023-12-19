7294e0fce69c3282494972996297f7da5ae6ec5a
samuel ramotshela <122275648+Ramotshela@users.noreply.github.com>
2023-12-19 22:31:30 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function sumOfEven(list){
+  let sum=0
+  for(let i=0;i<list.lenght;i++){
+     if (list[i] % 2 === 0) {
+            sum += list[i];
+        }
+}
+  return sum
+}
+
+const arr=[1,2,3,4,5,6,7,8,9]
+console.log(sumOfEven(arr))
